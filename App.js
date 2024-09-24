a169935a272405e79093b5517d30695cd9883428
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-24 20:59:02 +0900

    이미지 추가 및 파일 분리

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import ImageViewr from "./components/ImageViewr";
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewr placeholderImageSource={PlaceholderImage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
 });
---
+++ components/ImageViewr.js
@@ -0,0 +1,12 @@
+import { Image, StyleSheet } from "react-native";
+
+export default function ImageViewr({ placeholderImageSource }) {
+  return <Image source={placeholderImageSource} style={styles.image} />;
+}
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
